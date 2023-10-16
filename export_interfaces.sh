013207292f6b2256537b0cf0e635170ba366d5bc
Acélio Carvalho <95691973+AcelioCarvalho@users.noreply.github.com>
2023-10-16 16:28:15 -0300

    Update export_interfaces.sh

--- export_interfaces.sh
+++ export_interfaces.sh
@@ -10,24 +10,18 @@ echo "Interface de Rede Local, Interface de Rede Virtual, Grupo de Interface, Ut
 psql="psql -U postgres brcconfig -t -A -c"
 count=0
 
-for net in `$psql "select name from box_net_device where alias='f' and onboot='t' and obj_addr_id IS NOT NULL AND NOT name LIKE '%v%';" | sort`; do 
+for net in `$psql "select name from box_net_device where alias='t' or  virtual='t' and onboot='t' and obj_addr_id IS NOT NULL AND NOT name LIKE '%.%'"| sort`;do
 
     # Lista interfaces alias sobre as interfaces fisicas e VLAN
-    vip=`$psql "select name from box_net_device where alias='t' and onboot='t' and obj_addr_id IS NOT NULL and name like '$net:%';"`
-
-    # Listar interfaces Virtuais porem com descrição que contem  a palavra VIP.
-    if [ -z "$vip" ]; then
-
-        vip=`$psql "select name from box_net_device where virtual='t' and onboot='t' and obj_addr_id IS NOT NULL and name like '$net%v%' and full_description  ilike '%vip%';"`
-    fi
+    net_local=`echo "$net" | cut -d 'v' -f 1 | cut -d ':' -f 1 |sort`
 
     # Evitar listar interfaces sem VIP
-    if [ -n "$vip" ]; then
+    if [ -n "$net_local" ]; then
 
 	count=$((count + 1))
         
         # Exibi no farmato csv
         # Interface de Rede Local, Interface de Rede Virtual, Grupo de Interface, Utilizar protocolo UDP, Monitoramento, Chave de identificação
-        echo "$net, $vip, $count, 0, 0, $(openssl rand -base64 7 | tr -d '+/' | head -c 7)"
+        echo "$net_local, $net, $count, 0, 0, $(openssl rand -base64 7 | tr -d '+/' | head -c 7)"
     fi
 done
