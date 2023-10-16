0371890221d9f24171d69673f03b26c19dc2738d
Acélio Carvalho <95691973+AcelioCarvalho@users.noreply.github.com>
2023-10-16 16:48:19 -0300

    Update export_interfaces.sh

--- export_interfaces.sh
+++ export_interfaces.sh
@@ -6,14 +6,14 @@
 # Cabeçalho do arquivo modelo csv
 echo "Interface de Rede Local, Interface de Rede Virtual, Grupo de Interface, Utilizar protocolo UDP, Monitoramento, Chave de identificação"
 
-# Lista todas interfaces habilitadas e que contem IP.
+# Lista locais todas interfaces habilitadas e que contem IP.
 psql="psql -U postgres brcconfig -t -A -c"
 count=0
 
-for net in `$psql "select name from box_net_device where alias='t' or  virtual='t' and onboot='t' and obj_addr_id IS NOT NULL AND NOT name LIKE '%.%'"| sort`;do
+for vip in `$psql "select name from box_net_device where alias='t' or  virtual='t' and onboot='t' and obj_addr_id IS NOT NULL AND NOT name LIKE '%.%'"| sort`;do
 
-    # Lista interfaces alias sobre as interfaces fisicas e VLAN
-    net_local=`echo "$net" | cut -d 'v' -f 1 | cut -d ':' -f 1 |sort`
+    # Lista interfaces VIP
+    net_local=`echo "$vip" | cut -d 'v' -f 1 | cut -d ':' -f 1 |sort`
 
     # Evitar listar interfaces sem VIP
     if [ -n "$net_local" ]; then
@@ -22,6 +22,6 @@ for net in `$psql "select name from box_net_device where alias='t' or  virtual='
         
         # Exibi no farmato csv
         # Interface de Rede Local, Interface de Rede Virtual, Grupo de Interface, Utilizar protocolo UDP, Monitoramento, Chave de identificação
-        echo "$net_local, $net, $count, 0, 0, $(openssl rand -base64 7 | tr -d '+/' | head -c 7)"
+        echo "$net_local, $vip, $count, 0, 0, $(openssl rand -base64 7 | tr -d '+/' | head -c 7)"
     fi
 done
