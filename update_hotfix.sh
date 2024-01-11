24fd05e867cb2f62a20beea38ab85411b4fcd9e0
Acélio Carvalho <95691973+AcelioCarvalho@users.noreply.github.com>
2024-01-11 13:58:09 -0300

    Create update_hotfix.sh

---
+++ update_hotfix.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#Por: Acélio Carvalho
+
+version=`cut -d " " -f3 /etc/buildstamp | cut -d '.' -f 1-4`
+ha=`psql -U postgres brcconfig -t -A -c "select 1 from box_cluster_conf where type='disable';"`
+
+if [[ $ha != 1 ]];then
+   echo "Disable HA to continue"
+   exit 0
+fi
+
+baixar_hotfix() {
+        for hf in `curl -sS "https://docs.blockbit.com/display/RC/Hotfixes" | grep -oE '(https://s3.amazonaws.com/repo.blockbit.com/hotfix/([^\"]*)|https://shlink.blockbit.com/\w+)'`; do
+
+        if echo "$hf" | grep "shlink.blockbit.com" &>/dev/null; then
+                hf=`curl -L -w "%{url_effective}" -o /dev/null -s "$hf"`
+                hotfix=`echo "$hf" | cut -d "/" -f9 | sed "s/\%2F/_/g"`
+        else
+                hotfix=`echo "$hf" | cut -d "/"  -f 7`
+        fi
+        if echo "$hf" | grep "$version" &>/dev/null ; then
+        curl -s -X GET $hf -o $hotfix
+        /opt/omne/update_module/bin/omne-bb-update check -p $hotfix | cut -d "," -f 6,7
+        /opt/omne/update_module/bin/omne-bb-update install -p $hotfix | cut -d '"' -f 8
+        echo -e "\n"
+        fi
+        done
+}
+
+# Menu principal
+while true; do
+    echo -e "\nEscolha uma opção:"
+    echo "1. Instalar todos hotfix"
+    echo "2. Listar hotfix instalados"
+    echo "3. Remover último pacote"
+    echo "4. Remover todos hotfix"
+    echo -e "5. Sair\n"
+
+    read choice
+
+        case $choice in
+
+        1)
+                baixar_hotfix
+                echo -e "Todos Hotfix instalados\n"
+                ;;
+        2)
+                 echo -e "\nid| description"
+                 sqlite3 /opt/omne/update_module/db/database_update.sqlite "select id,description from updates;"
+                 echo "              "
+                ;;
+        3)
+
+                last_hf=`sqlite3 /opt/omne/update_module/db/database_update.sqlite --separator ' - ' "select id,description from updates ORDER BY id DESC LIMIT 1;"`
+
+                echo -e "\nRemovendo hotfix: $last_hf"
+                /opt/omne/update_module/bin/omne-bb-update rollback &>/dev/null
+                echo  -e "Concluído\n"
+
+                ;;
+
+        4)
+                desc_hf=`sqlite3 /opt/omne/update_module/db/database_update.sqlite --separator ' - ' "select id,description from updates ORDER BY id DESC;"`
+                id=`sqlite3 /opt/omne/update_module/db/database_update.sqlite "select id from updates ORDER BY id DESC;"`
+
+                for id_hf in $id; do
+                        desc_hf=`sqlite3 /opt/omne/update_module/db/database_update.sqlite --separator ' - ' "select description from updates where id = $id_hf ;"`
+
+                        echo -e "\nRemovendo hotfix: $id_hf - $desc_hf"
+
+
+                        /opt/omne/update_module/bin/omne-bb-update rollback | cut -d '"' -f 8
+                done
+                echo -e "\nTodos hotfix removidos"
+                ;;
+        5)
+                echo "Saindo ..."
+                break
+                ;;
+
+        *)
+            echo "Opção inválida. Tente novamente."
+            ;;
+    esac
+done
