868f102f61f6ec764f8e4f3efac73daeb607f6b2
Acélio Carvalho <95691973+AcelioCarvalho@users.noreply.github.com>
2024-02-05 16:24:47 -0300

    Update sif.sh

--- sif.sh
+++ sif.sh
@@ -3,29 +3,28 @@
 # Nome: Scanning Immutable Files
 # Objetivo: Verificar arquivos imutaveis antes de qualquer atualizações do BBOS.
 
-# Encontrar arquivos e salvar em files.txt
 find /etc/ /usr/lib/systemd/system/ /opt/addons/ /opt/omne/apply/ /opt/omne/conf/ /opt/omne/bin/ /opt/omne/admin/ajax/ /opt/omne/admin/apps/ ! -path /etc/resolv.conf -type f -not -name "*.png" > files.txt
+
+messages=""
 
 total_files=$(wc -l < files.txt)
 current_file=0
 
 echo "Verificando arquivos ..."
 
-# Loop através dos arquivos em files.txt
 while IFS= read -r find; do
     ((current_file++))
-
-    # Exibir barra de progresso
+    
     percentage=$((current_file * 100 / total_files))
     echo -ne "Progresso: [$percentage%]\r"
 
     if [ "$(lsattr -d "$find" | awk '{print $1}' | grep "i")" ]; then
-        echo "Arquivo imutável: $find"
+        messages+="Arquivo imutável: $find"$'\n'
     fi
 done < files.txt
 
-# Adicionar nova linha após a barra de progresso
 echo
 
-# Limpar o arquivo temporário
+echo "$messages"
+
 rm -f files.txt
